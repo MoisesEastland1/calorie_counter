36491cf5a5428770390cc713146a78ee16bd1cd8
MoisesEastland1 <meastland977@gmail.com>
2024-03-12 16:42:00 -0500

    commented out cleanInputString funct.

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ const clearButton = document.getElementById('clear');
 const output = document.getElementById('output');
 let isError = false;
 
-function cleanInputString(str){
-    const strArray = str.split('');
-    const cleanStrArray = [];
-    for(let i=0; i < strArray.length; i++){
-        if (!['+','-',' '].includes(strArray[i]))
-        cleanStrArray.push(strArray[i]);
-    }
-}
+//function cleanInputString(str){
+   // const strArray = str.split('');
+   // const cleanStrArray = [];
+   // for(let i=0; i < strArray.length; i++){
+        //if (!['+','-',' '].includes(strArray[i]))
+       // cleanStrArray.push(strArray[i]);
+   // }
+//}
